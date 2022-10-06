a316aa4c5e4d7ccce3cf95649f31e6099e2eb678
Pawel Macyszyn <macyszyn78@gmail.com>
2022-10-06 21:30:21 +0200

    Simple_Countdown_Timer

---
+++ Simple_Countdown_Timer/script.js
@@ -0,0 +1,26 @@
+// alert('is made by Pawel Macyszyn')
+
+
+const sec = 1000 // ms
+const startingMinutes = 10
+let time = startingMinutes * 60
+
+const countdownEl = document.getElementById('countdown')
+
+
+setInterval(updateCountdown, sec)
+
+
+function updateCountdown() {
+    let minutes = Math.floor(time / 60)
+    let seconds = time % 60
+
+
+    minutes = minutes < 10 ? '0' + minutes: minutes
+
+    seconds = seconds < 10 ? '0' + seconds: seconds
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`
+    time--
+
+}
