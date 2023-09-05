2f0c364e3ffee41b853051b0faa9dd859de48bf6
Pawel Macyszyn <macyszyn78@gmail.com>
2023-09-05 17:06:42 +0200

    static asets

--- NODE-EXPRESS-COURSE/02-express-tutorial/app.js
+++ NODE-EXPRESS-COURSE/02-express-tutorial/app.js
@@ -1,50 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 5000;
 
-// ---------------  favicon.ico  ----------------
-const mainRoutes = require("./favicon/mainRoutes.js");
-app.use(mainRoutes);
-// ---------------  favicon.ico  ----------------
+app.use(express.static("./navbar-app"));
 
 app.get("/", (req, res) => {
-  console.log("user hit the resource");
-  res.send("Home Page");
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("Contakt Page");
-});
-
-// app.all("*", (req, res) => {
-//   res.send("<h1>resource not found</h1>");
-// });
 app.all("*", (req, res) => {
-  res.status(404).send(`
-    <html>
-      <head>
-        <title>RESOURCE NOT FOUND</title>
-      </head>
-      <body>
-        <h1>resource not found</h1>
-      </body>
-    </html>
-  `);
+  res.status(404).send("<h1>resource not found</h1>");
 });
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}\n`);
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-
-// app.all
-// app.use
-// app.listen
