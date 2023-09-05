03e77be38d05319998b5234a46a4079f0fdeb4b7
Pawel Macyszyn <macyszyn78@gmail.com>
2023-09-05 09:45:45 +0200

    express  app.get ...

--- NODE-EXPRESS-COURSE/02-express-tutorial/app.js
+++ NODE-EXPRESS-COURSE/02-express-tutorial/app.js
@@ -1,1 +1,50 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+const port = 5000;
+
+// ---------------  favicon.ico  ----------------
+const mainRoutes = require("./mainRoutes");
+app.use(mainRoutes);
+// ---------------  favicon.ico  ----------------
+
+app.get("/", (req, res) => {
+  console.log("user hit the resource");
+  res.status(200).send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contakt Page");
+});
+
+// app.all("*", (req, res) => {
+//   res.send("<h1>resource not found</h1>");
+// });
+app.all("*", (req, res) => {
+  res.status(404).send(`
+    <html>
+      <head>
+        <title>RESOURCE NOT FOUND</title>
+      </head>
+      <body>
+        <h1>resource not found</h1>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}\n`);
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+
+// app.all
+// app.use
+// app.listen
