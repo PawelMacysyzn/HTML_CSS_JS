577600c6812d04f631772ab781a829e6a76fbfc6
Pawel Macyszyn <macyszyn78@gmail.com>
2023-09-13 11:39:18 +0200

    Favicon stuff

--- NODE-EXPRESS-COURSE/02-express-tutorial/app.js
+++ NODE-EXPRESS-COURSE/02-express-tutorial/app.js
@@ -1,18 +1,35 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const port = 5000;
 
-app.use(express.static("./navbar-app"));
+
+
+// ---------------  favicon.ico  ----------------
+const mainRoutes = require("./favicon/mainRoutes");
+app.use(mainRoutes);
+// ---------------  favicon.ico  ----------------
+
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+  res.send("Home");
 });
 
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>resource not found</h1>");
+app.get("/about", (req, res) => {
+  res.send("About");
 });
+
+app.all("/about", (req, res) => {
+  res.send("About");
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}\n`);
 });
+
+
+// app.param
